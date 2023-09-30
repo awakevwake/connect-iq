77fe9af92d0ac4f52725e299e3b7bdbd5f264c04
awakevwake <awakevwake@outlook.com>
2023-09-30 20:19:49 +0530

    Initial : Application setup

---
+++ request-response-repo-svc/src/main/java/com/awakevwake/RequestResponseDaoApp.java
@@ -0,0 +1,12 @@
+package com.awakevwake;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RequestResponseDaoApp {
+
+    public static void main(String[] args){
+        SpringApplication.run(RequestResponseDaoApp.class,args);
+    }
+}
---
+++ user-repo-svc/src/main/java/com/awakevwake/UserDaoApp.java
@@ -0,0 +1,12 @@
+package com.awakevwake;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserDaoApp {
+
+    public static void main(String[] args){
+        SpringApplication.run(UserDaoApp.class,args);
+    }
+}
